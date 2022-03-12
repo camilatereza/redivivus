8e2e62080fab6717a3e9faeb96e29594d530ab57
Luis Vitor <luis.vitor@dcx.ufpb.br>
2022-03-12 13:11:11 -0300

    refs #5 	- criação das janelas que mostraram as instruções e as jogadas estrelas

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Wrap, Flex, Button, IconButton  } from '@react-native-material/core';
+import { Wrap, Flex, Button, IconButton, Box, Provider, VStack  } from '@react-native-material/core';
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { useFonts } from '@expo-google-fonts/inter';
 import AppLoading from 'expo-app-loading';
+import {
+  Dialog,
+  DialogHeader,
+  DialogContent,
+  DialogActions,
+} from "@react-native-material/core";
+import { instructions, sortInstructions } from './src/assets/constants/instructions';
+import { sortStarResource } from './src/assets/constants/star.resources';
 
 export default function App() {
+  
+  // zero -> nenhum modal visivel
+  // um -> apenas intruções visiveis
+  // dois -> apenas recompensas visiveis
+  const [visible, setVisible] = useState(0);
+  const [instructions, setInstructions] = useState(sortInstructions());
+  const [star,setStar]= useState(sortStarResource());
+
+
   let [fontsLoaded] = useFonts({
     'Lobster-Regular': require('./src/assets/fonts/Lobster-Regular.ttf')
   });
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+
+
+
   return (
+    <Provider>
       <Wrap justify="center" items="center" style={styles.wrapper}>
         <Flex w={'40%'} h={'38%'} mt={'20%'} mb={'20%'}>
           <TouchableOpacity
@@ -24,8 +46,8 @@ export default function App() {
           style={[styles.button,styles.sertanejo]}
         ><Text style={styles.text}>Sertanejo</Text></TouchableOpacity></Flex>
         <Flex w={'100%'} h={'5%'} center="true">
-          <Button compact uppercase variant="outlined" title="Play"></Button>
-          <IconButton icon={props => <Icon name="star" size={78} {...props} />} color="primary" />
+          <Button onPress={() => setVisible(1)} compact uppercase variant="outlined" title="Play"></Button>
+          <IconButton onPress={() => setVisible(2)} icon={props => <Icon  name="star" size={78} {...props} />} color="primary" />
         </Flex>
         <Flex w={'40%'} h={'38%'} mt={'20%'}  mb={'20%'} >
           <TouchableOpacity
@@ -36,6 +58,56 @@ export default function App() {
           style={[styles.button,styles.mpb]}  
         ><Text style={styles.text}>MPB</Text></TouchableOpacity></Flex>
       </Wrap>
+      <Dialog visible={visible === 1} onDismiss={() => setVisible(0)}>
+          <DialogHeader title="Escolha 3 jogadas" />
+          <DialogContent>
+            <VStack spacing={2} p={20} >
+              {
+                instructions.map(
+                (item, index) => {
+                  return <Flex inline key={index} >
+                      <Icon name={item.icon} size={16}></Icon>
+                      <Text>{item.text}</Text>
+                    </Flex>
+                })}
+            </VStack>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              title="Ok"
+              compact
+              variant="text"
+              onPress={() => {
+                setVisible(0);
+                setTimeout(() =>setInstructions(sortInstructions()),1000)
+              }}
+            />
+          </DialogActions>
+        </Dialog>
+      <Dialog visible={visible === 2} onDismiss={() => setVisible(0)}>
+        <DialogHeader title="Jogada Estrela" />
+        <DialogContent>
+          <Flex>
+            {
+              <Box spacing={2} p={20}>
+                  <Text>{star}</Text>
+              </Box>
+              }
+          </Flex>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            title="Ok"
+            compact
+            variant="text"
+            onPress={() => {
+              setVisible(0);
+              setTimeout(() => setStar(sortStarResource()),1000)
+            }}
+          />
+        </DialogActions>
+      </Dialog>
+      </Provider>
   );
 }
 
---
+++ src/assets/constants/instructions.js
@@ -0,0 +1,15 @@
+import { random } from "../../utils/sort.util";
+
+export const instructions = [
+    {icon: 'rotate-left', text: 'Vire para a esquerda'},
+    {icon: 'rotate-right', text: 'Vire para a direita'},
+    {icon: 'arrow-up-bold', text: 'Vá em frente'},
+]
+
+export function sortInstructions() {
+    let inst = [];
+    for(i=0; i < 5; i++){
+        inst.push(instructions[random(0,2)]);
+    }
+    return inst;
+}
